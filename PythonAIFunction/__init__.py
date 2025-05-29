61262892e205b811f085bf4c07cd0e876784f19d
Diana Fernández Madrigal <dfernandezm@onesec.mx>
2025-05-29 10:47:39 -0600

    creación de readme

---
+++ PythonAIFunction/__init__.py
@@ -0,0 +1,129 @@
+from ast import keyword
+from email import message
+from email.message import Message
+import logging
+import azure.functions as func
+import os
+from msrest import ServiceClient
+import pytesseract
+from PIL import Image
+from pdf2image import convert_from_path
+from PIL import Image
+from numpy import asarray
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobClient
+from azure.storage.blob import BlobServiceClient
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+import time
+import json
+import random
+
+
+def main(msg: func.ServiceBusMessage):
+    logging.info('Python ServiceBus queue trigger processed message: %s', msg.get_body().decode('utf-8'))
+
+    print("CONFIGURATION VALUES:")
+    #URL of the storage account containing the files
+    storage_url = os.environ["STORAGE_ACCT_URL"]   
+    print(storage_url)
+    #Name of the blob container with the source fies 
+    source_container_name = os.environ["SOURCE_CONTAINER_NAME"]  
+    print(source_container_name) 
+    #Name of the blob container where the trimmed, single page JPEG will be saves
+    destination_container_name = os.environ["DESTINATION_CONTAINER_NAME"]   
+    print(destination_container_name) 
+    #Name of the queue to send message to
+    destination_queue_name = os.environ.get("DESTINATION_QUEUE_NAME", "")
+    print(destination_queue_name) 
+
+    servicebus_conn_string = os.environ["SERVICEBUS_CONNECTION"]   
+
+    #Comma separated list of keywords to find in docs
+    keywords_list = os.environ["KEYWORDS_LIST"].split(",")   
+    print(keywords_list)
+
+
+    #Retrieve the source file from Azure storage    
+    token_credential = DefaultAzureCredential()
+    serviceClient = BlobServiceClient(account_url=storage_url, credential=token_credential)
+    message_file_name = msg.get_body().decode('utf-8')
+    source_blob_client = serviceClient.get_blob_client(container=source_container_name, blob=message_file_name)
+ 
+    #save the file locally
+    filePath = "/home/"+message_file_name
+    with open(filePath, "wb") as my_blob:
+       download_stream = source_blob_client.download_blob()
+       my_blob.write(download_stream.readall())
+    print("file was saved locally")
+    print(filePath)
+
+    doc = convert_from_path(filePath, fmt='jpeg')
+
+    path, fileName = os.path.split(filePath)
+    fileBaseName, fileExtension = os.path.splitext(fileName)
+    
+    start = time.process_time()
+
+
+    #loop through the pages in the PDF
+    for page_number, page_data in enumerate(doc):
+        #perform Tesseract OCR to extract text
+        txt = pytesseract.image_to_string(Image.fromarray(asarray(page_data))).encode("utf-8")
+
+        #look for matching keyword and if found continue processing
+        for keyword in keywords_list:
+            if keyword in txt.decode("utf-8"):
+                print(keyword + " string is present")
+                temp_file_name = fileBaseName+ "-page-"+ str(page_number) +'.jpg'
+                Image.fromarray(asarray(page_data)).save('/home/'+temp_file_name)
+                print('Image created \n')
+                print("Page # {} - {}".format(str(page_number),txt))
+
+                #upload to Azure storage
+                try:
+                    dest_blob_client = serviceClient.get_blob_client(container=destination_container_name, blob=temp_file_name)
+                    with open('/home/'+temp_file_name, "rb") as my_dest_blob:
+                        dest_blob_client.upload_blob(my_dest_blob)
+                    print("File '" + temp_file_name + "' uploaded to storage")
+                except Exception as e:
+                    print("ERROR: Failed to upload '" + temp_file_name + "' to storage: " + e.message)
+
+                #send the file message to the destination queue (if provided)
+                try:
+                    if(len(destination_queue_name) > 0):
+                        with ServiceBusClient.from_connection_string(servicebus_conn_string) as sb_client:
+                            with sb_client.get_queue_sender(destination_queue_name) as q_sender:
+                                msgJson = {
+                                    "FileName" : temp_file_name,
+                                    "ContainerName" : destination_container_name,
+                                    "RecognizerIndex" : random.randint(0,9)                
+                                    }
+                                message = ServiceBusMessage(json.dumps(msgJson))
+                                q_sender.send_messages(message)
+                    else:
+                        print("No destination queue name provided")
+                except Exception as e:
+                    print("ERROR: Failed to send message for file'" + temp_file_name + "' to queue: " + e.message)
+                
+                #Delete the temp jpeg file
+                try:
+                    if os.path.exists('/home/'+temp_file_name):
+                        os.remove('/home/'+temp_file_name)
+                        print("Deleted temp file'" + temp_file_name + "' ")
+                except Exception as e:
+                    print("ERROR: Failed to delete local temp file'" + temp_file_name + "' " + e.message)
+                break
+
+            else:
+                print("No matches of '"+ keyword +"' on page '"+ str(page_number) + "' of file '" + message_file_name + "'")
+
+            #Delete the temp PDF file
+            try:
+                if os.path.exists(filePath):
+                    os.remove(filePath)
+                    print("Deleted original source file'" + filePath + "' ")
+            except Exception as e:
+                print("ERROR: Failed to delete original source file'" + filePath + "' " + e.message)
+            break
+
+    print("TOTAL PROCESS TIME: " + str(time.process_time() - start))
--- README.md
+++ README.md
@@ -1,20 +1,64 @@
-# Introduction 
-TODO: Give a short introduction of your project. Let this section explain the objectives or the motivation behind this project. 
+# Python AI Function
 
-# Getting Started
-TODO: Guide users through getting your code up and running on their own system. In this section you can talk about:
-1.	Installation process
-2.	Software dependencies
-3.	Latest releases
-4.	API references
+This repository is offered to demonstrate a set of resources that will allow you to leverage an Azure Function that uses Tesseract OCR to identify pages within a PDF document that contains the desired text. It will then take that page from the document and save it as a JPEG image in Azure Storage.  When used in conjunction with the [High Throughput Form Recognizer](https://github.com/mmckechney/HighThroughputFormRecognizer) it will allow you to perform very efficient and scalable file processing and form recognition for millons of documents.  
 
-# Build and Test
-TODO: Describe and show how to build your code and run the tests. 
+**NOTE**: If you plan on using this solution in conjunction with the [High Throughput Form Recognizer](https://github.com/mmckechney/HighThroughputFormRecognizer), it is suggested to run the deployment script from that solution with the same `appName`, `location` and `myPublicIp` values that you use to run the `deploy.ps1` in this solution. This will ensure that they work well together.
 
-# Contribute
-TODO: Explain how other users and developers can contribute to make your code better. 
+## Features
 
-If you want to learn more about creating good readme files then refer the following [guidelines](https://docs.microsoft.com/en-us/azure/devops/repos/git/create-a-readme?view=azure-devops). You can also seek inspiration from the below readme files:
-- [ASP.NET Core](https://github.com/aspnet/Home)
-- [Visual Studio Code](https://github.com/Microsoft/vscode)
-- [Chakra Core](https://github.com/Microsoft/ChakraCore)
+The solution leverages the following Azure services:
+
+- **Azure Blob Storage** with two containers
+  - `rawfile` - storage for the raw multipage PDF documents
+  - `incoming` - storage for the single page JPEG images  (named `incoming` to match the expected source container in  [High Throughput Form Recognizer](https://github.com/mmckechney/HighThroughputFormRecognizer))
+- **Azure Service Bus** with two queues
+  - `rawqueue` - identified the "raw" multipage PFD documents that need processing
+  - `formqueue` - where a new queue message will be placed to hand off to the [High Throughput Form Recognizer](https://github.com/mmckechney/HighThroughputFormRecognizer) processing
+- **Azure Functions**
+  - `PythonAIFunction` - Containerized Python function that will use the `rawqueue` to identify PDF files for processing, perform Tesseract OCR to search for the specified keywords, then when found, create a JPEG of that page and save it to the `trimmed` storage container
+- **Azure Container Registry** - because this function is deployed as a Docker container, it will use the container registry as its deployment source
+
+
+## Configuration
+
+The solution leverages Azure Managed Identity for connections to Azure Storage and for sending messages to the `formqueue`. This identity needs to have `Storage Blob Data Contributor` role to the stogate account and `Azure Service Bus Data Owner` role to the Service Bus. 
+
+The function also require the following App Settings values:
+
+- `KEYWORD_LIST` - comma separated list of keywords to find in the PDF documents. The matching will be case sensitive
+- `SERVICEBUS_CONNECTION` - connection string for the Service Bus. This will be used by the function binding
+- `STORAGE_ACCT_URL` - the URL for the storage account that will be used
+- `SOURCE_CONTAINER_NAME` - the name of the blob container that will have the source PDF documents
+- `DESTINATION_CONTAINER_NAME` - the name of the blob container that the trimmed JPEG files will be saved
+
+The `function.json` file will need to have the `queueName` has the appropriate name of the queue that is located in the service bus
+
+## Get Started
+
+To try out the sample end-to-end process, you will need:
+
+- An Azure subscription that you have privileges to create resources. 
+- Your public IP address. You can easily find it by following [this link](https://www.bing.com/search?q=what+is+my+ip).
+- Have the [Azure CLI installed](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli).
+
+### Running deployment script
+
+1. Login to the Azure CLI:  `az login`
+2. Run the deployment command
+
+    ``` PowerShell
+    .\deploy.ps1 -appName "<less than 6 characters>" -location "<azure region>" -myPublicIp "<your public ip address>"
+
+    ```
+
+    This will create all of the azure resources needed to run the solution.
+
+  
+### Rebuilding the Container Image
+
+The container image is built and added to the Azure Container Registry as part of the deploymet script. However if you need to rebuild the image, use the Azure Container Registry [build tasks](https://docs.microsoft.com/en-us/azure/container-registry/container-registry-tasks-overview) command below. Because the function app is configured with Continuous Delivery, the new image will be automatically retrieved.
+``` bash
+az acr build --registry "<registry name>" --image "<name>:<tag>" --file ./DOCKERFILE . --no-logs
+```
+
+
